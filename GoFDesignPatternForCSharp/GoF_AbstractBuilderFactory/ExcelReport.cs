83e09b932ed9d989c1f17e067fc0abcc5e148d1d
Lee-juNu <lee-junwoo@msen.jp>
2024-07-09 21:43:09 +0900

    AbstractBuilderFactoryPattern  生成

---
+++ GoFDesignPatternForCSharp/GoF_AbstractBuilderFactory/ExcelReport.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoF_AbstractBuilderFactory
+{
+    public class ExcelReport : IReport
+    {
+        public void GenerateReport()
+        {
+            Console.WriteLine("generating Excel Report...");
+        }
+    }
+}
---
+++ GoFDesignPatternForCSharp/GoF_AbstractBuilderFactory/Main.cs
@@ -0,0 +1,19 @@
+﻿// generate basic Main Class
+using System;
+
+namespace GoF_AbstractBuilderFactory
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            IReportFactory pdfFactory = new PDFReportFactory();
+            IReport pdfReport = pdfFactory.CreateReport();
+            pdfReport.GenerateReport();
+        
+            IReportFactory excelFactory = new ExcelReportFactory();
+            IReport excelReport = excelFactory.CreateReport();
+            excelReport.GenerateReport();
+        }
+    }
+}
